bffaa6c3b64c9e9efb5dfea8d045c31aa079d810
Silski Michal (Consultant) <michal.silski@consultant.volvo.com>
2022-05-02 10:39:00 +0200

    added pictrue

--- frontend/src/Copmonents/App.js
+++ frontend/src/Copmonents/App.js
@@ -59,7 +59,7 @@ const App = (params) => {
     <Container maxWidth="md">
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Typography variant="h5">New Version Of ToDo App (image version {IMAGE_VERSION})</Typography>
+          <Typography variant="h5">ToDo App (image version {IMAGE_VERSION})</Typography>
         </Grid>
         <Grid item xs={12}>
           <TodoForm onAdd={onSubmit} />
